ab5501ec1eb84aa4d750a583679228c2b865577c
NTDHaruto <dung11112003qng@gmail.com>
2024-01-11 22:55:45 +0700

    Revert "109 110 (23.8.2023)"
    This reverts commit 3d4462f4867db1824a4afb5fea4d38184c841f72.

--- CapNhatBai/Bai 109.py
+++
@@ -1,8 +0,0 @@
-# Bài 109: Viết chương trình in bảng cửu chương ra màn hình
-
-for i in range(1,10,1):
-    for j in range(2,11,1):
-        if j==10:
-            print()
-        else:
-            print('{0} x {1} = {2:2} |'.format(j,i,j*i),end='')
